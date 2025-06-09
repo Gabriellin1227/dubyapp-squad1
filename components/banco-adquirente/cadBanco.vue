a0ac875e344c826e16c53052bacd0a9be478f577
bieeel <gabriel.lin@souunit.com.br>
2025-06-09 16:42:45 -0300

    inicio - cadastrar bancos

---
+++ components/banco-adquirente/cadBanco.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container-principal">
+        <div class="container-conteudo">
+            <div class="titulo">
+                <h1>Cadastrar Banco</h1>
+            </div>
+            <div class="container-itens">
+                <div class="nome"></div>
+                <div class="razao-social"></div>
+                <div class="cnpj"></div>
+                <div class="codbanco"></div>
+                <div class="agencia"></div>
+                <div class="conta"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container-conteudo {
+    width: 624px;
+    height: 504px;
+    background-color: white;
+}
+</style>
