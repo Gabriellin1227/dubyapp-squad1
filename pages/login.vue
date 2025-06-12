65f0196613c7c7af2d2f7fea7f1f3e83bcd844ca
Abraão Reis <andradeabraao23@gmail.com>
2025-06-12 15:50:38 -0300

    login module done

--- pages/login.vue
+++ pages/login.vue
@@ -13,18 +13,48 @@ const form = reactive({
     password: ''
 });
 
+const showAlertModal = ref(false);
+
+const closeModal = () => {
+    showAlertModal.value = false;
+};
+
 function handleLogin() {
     const success = auth.login(form.email, form.password);
 
     if (success) {
         router.push('/');
     } else {
-        alert('Login inválido'); // aqui posso adicionar o modal de aviso
+        showAlertModal.value = true;
     }
 }
 </script>
 
 <template>
+    <div v-if="showAlertModal" class="modal-overlay">
+        <dialog open class="modal-dialog">
+            <div class="modal-content">
+                <div class="alert-icon-extern">
+                    <div class="alert-icon-medium">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 48 48">
+                            <g fill="none">
+                                <path stroke="#412884" stroke-linejoin="round" stroke-width="3" d="M24 44a19.94 19.94 0 0 0 14.142-5.858A19.94 19.94 0 0 0 44 24a19.94 19.94 0 0 0-5.858-14.142A19.94 19.94 0 0 0 24 4A19.94 19.94 0 0 0 9.858 9.858A19.94 19.94 0 0 0 4 24a19.94 19.94 0 0 0 5.858 14.142A19.94 19.94 0 0 0 24 44Z" />
+                                <path fill="#412884" fill-rule="evenodd" d="M24 37a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5" clip-rule="evenodd" />
+                                <path stroke="#412884" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M24 12v16" />
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+                <div class="description-container">
+                    <div class="alert-title">Credenciais inválidas</div>
+                    <div class="alert-subtitle">Corrija as informações inseridas e tente novamente.</div>
+                </div>
+                <button @click="closeModal" class="alert-button">
+                    Fechar
+                </button>
+            </div>
+        </dialog>
+    </div>
     <div class="container">
         <div id="content-box">
             <div class="logo">
@@ -79,182 +109,182 @@ function handleLogin() {
 
     max-height: 576px;
     max-width: 864px;
-    
+
     width: 100%;
     height: 100%;
-    
+
     padding: 24px 136px;
-    
+
     background-color: var(--background);
-    
+
     border-radius: 28px;
     box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
-}
 
-#content-box {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-lg);
+    #content-box {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-lg);
 
-    max-height: 520px;
-    max-width: 592px;
-    
-    height: 100%;
-    width: 100%;
-    
-    padding: var(--spacing-md);
-}
+        max-height: 520px;
+        max-width: 592px;
 
-/* LOGO */
+        height: 100%;
+        width: 100%;
 
-.logo {
-    display: flex;
-    justify-content: center;
+        padding: var(--spacing-md);
+    }
 
-    font-size: var(--spacing-md);
-}
+    /* LOGO */
 
-.logo img {
-    height: 80px;
-    width: 420px;
-}
+    .logo {
+        display: flex;
+        justify-content: center;
 
-/* FORM */
+        font-size: var(--spacing-md);
+    }
 
-.form {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-md);
-    
-    height: 392px;
-    max-width: 560px;
+    .logo img {
+        height: 80px;
+        width: 420px;
+    }
 
-    width: 100%;
-}
+    /* FORM */
 
-form {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-sm);
-}
+    .form {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-md);
 
-form > div {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-sm);
-}
+        height: 392px;
+        max-width: 560px;
 
-label {
-    display: flex;
-    align-items: center;
+        width: 100%;
+    }
 
-    height: 24px;
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-sm);
+    }
 
-    color: var(--texto);
-    font-family: var(--fonte-padrao), sans-serif;
-    font-size: var(--font-md);
-}
+    form>div {
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-sm);
+    }
 
-input {
-    height: 56px;
-    width: 100%;
+    label {
+        display: flex;
+        align-items: center;
 
-    padding: var(--spacing-slg) 0;
-    
-    color: var(--texto);
-    text-indent: var(--spacing-sm);
-    font-family: var(--fonte-padrao), sans-serif;
-    font-size: var(--font-md);
-    
-    background-color: var(--input);
+        height: 24px;
 
-    border: none;
-    border-radius: 10px;
+        color: var(--texto);
+        font-family: var(--fonte-padrao), sans-serif;
+        font-size: var(--font-md);
+    }
 
-    outline: 2px solid transparent;
-    transition: outline 0.3s ease-in-out;
-}
+    input {
+        height: 56px;
+        width: 100%;
 
-input::placeholder {
-    color: rgba(18, 40, 64, 0.5);
-    text-indent: var(--spacing-sm);
-    font-size: var(--font-snd);
-}
+        padding: var(--spacing-slg) 0;
 
-input:focus {
-    background-color: var(--background);
+        color: var(--texto);
+        text-indent: var(--spacing-sm);
+        font-family: var(--fonte-padrao), sans-serif;
+        font-size: var(--font-md);
 
-    outline: 2px solid var(--principal);
-}
+        background-color: var(--input);
 
-#botaoLogin {
-    height: 56px;
-    width: 100%;
+        border: none;
+        border-radius: 10px;
 
-    padding: var(--spacing-sm) 0;
+        outline: 2px solid transparent;
+        transition: outline 0.3s ease-in-out;
+    }
 
-    color: var(--background);
-    line-height: 20px;
-    font-family: var(--fonte-padrao), sans-serif;
-    font-size: var(--font-md);
-    font-weight: 700;
+    input::placeholder {
+        color: rgba(18, 40, 64, 0.5);
+        text-indent: var(--spacing-sm);
+        font-size: var(--font-snd);
+    }
 
-    background-color: var(--principal);
-    border: none;
-    border-radius: 10px;
-    cursor: pointer;
+    input:focus {
+        background-color: var(--background);
 
-    outline: 2px solid transparent;
-    transition: outline 0.3s ease-in-out;
-}
+        outline: 2px solid var(--principal);
+    }
 
-#botaoLogin:hover {
-    background-color: var(--comp-1);
-}
+    #botaoLogin {
+        height: 56px;
+        width: 100%;
 
-#botaoLogin:focus {
-    outline: 2px solid var(--comp-4);
-}
+        padding: var(--spacing-sm) 0;
 
-/* LINKS & UTILITARIOS */
+        color: var(--background);
+        line-height: 20px;
+        font-family: var(--fonte-padrao), sans-serif;
+        font-size: var(--font-md);
+        font-weight: 700;
 
-#actionButtons {
-    display: flex;
-    justify-content: space-between;
+        background-color: var(--principal);
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
 
-    height: 24px;
-}
+        outline: 2px solid transparent;
+        transition: outline 0.3s ease-in-out;
+    }
 
-#actionButtons a {
-    display: flex;
-    align-items: center;
+    #botaoLogin:hover {
+        background-color: var(--comp-1);
+    }
 
-    height: 24px;
+    #botaoLogin:focus {
+        outline: 2px solid var(--comp-4);
+    }
 
-    color: var(--comp-1);
-    font-family: var(--fonte-padrao), sans-serif;
-    font-size: var(--font-md);
+    /* LINKS & UTILITARIOS */
 
-    border-radius: 2px;
+    #actionButtons {
+        display: flex;
+        justify-content: space-between;
 
-    outline: 2px solid transparent;
-    transition: outline 0.3s ease-in-out;
-}
+        height: 24px;
+    }
 
-#actionButtons span:hover {
-    color: var(--comp-3);
-    text-decoration: underline;
-}
+    #actionButtons a {
+        display: flex;
+        align-items: center;
 
-#actionButtons a:focus {
-    outline: 2px solid var(--principal);
-}
+        height: 24px;
 
-#remember-container {
-    display: flex;
-    gap: 8px;
+        color: var(--comp-1);
+        font-family: var(--fonte-padrao), sans-serif;
+        font-size: var(--font-md);
 
-    color: var(--principal);
+        border-radius: 2px;
+
+        outline: 2px solid transparent;
+        transition: outline 0.3s ease-in-out;
+    }
+
+    #actionButtons span:hover {
+        color: var(--comp-3);
+        text-decoration: underline;
+    }
+
+    #actionButtons a:focus {
+        outline: 2px solid var(--principal);
+    }
+
+    #remember-container {
+        display: flex;
+        gap: 8px;
+
+        color: var(--principal);
+    }
 }
 
 .custom-checkbox {
@@ -310,5 +340,104 @@ input:focus {
 .checkbox-label {
   margin-left: 8px;
   color: var(--texto);
+}
+
+.modal-overlay {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    z-index: 999;
+
+    height: 100vh;
+    width: 100vw;
+
+    background: rgba(0, 0, 0, 0.3);
+}
+
+.modal-dialog {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    all: unset;
+    z-index: 1000;
+
+    width: min-content;
+    
+    padding: 24px 56px;
+
+    background-color: var(--background);
+
+    border-radius: 20px;
+}
+
+.modal-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+
+    height: 100%;
+    width: 384px;
+
+    .alert-icon-extern {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 56px;
+        width: 56px;
+        
+        background-color: rgba(65, 40, 132, 0.15);
+        
+        border-radius: 50%;
+        
+        .alert-icon-medium {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            height: 44px;
+            width: 44px;
+
+            background-color: rgba(65, 40, 132, 0.15);
+
+            border-radius: 50%;
+        }
+    }
+
+    .description-container {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+
+        text-align: center;
+
+        .alert-title {
+            color: var(--principal);
+            font-size: 28px;
+            font-weight: 600;
+        }
+
+        .alert-subtitle {
+            color: var(--texto);
+            font-size: 16px;
+        }
+    }
+
+    .alert-button {
+        height: 40px;
+        width: 160px;
+
+        color: var(--background);
+
+        background-color: var(--principal);
+
+        border: 0;
+        border-radius: 10px;
+    }
 }
 </style>
